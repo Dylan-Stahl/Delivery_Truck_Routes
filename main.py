932a8ddc229bfa6ac59e07eef9aacdc3084d8395
Dylan Stahl <73616131+Dylan-Stahl@users.noreply.github.com>
2021-06-20 22:41:31 -0700

    nearest neighbor algorithm works with test data locations.

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from data_loader import load_packages
 from data_loader import package_hash
 from truck import *
-
+from nearest_neighbor import Graph, Vertex, nearest_neighbor, truck_three_graph
 # Name: Dylan Stahl
 # Student ID: 002996740
 
@@ -23,7 +23,39 @@ print(truck_two)
 
 truck_three = truck_hash.search(3)
 print(truck_three)
-for e in truck_three.package_array:
+
+
+# I will create separate graphs for each truck
+# for e in truck_three.package_array:
     # print for now, but eventually add to nearest neighbor algorithm
+    # print(e.address)
+    # new_vertex = Vertex(e.address)
+    # truck_three_graph.add_vertex(new_vertex)
+    # add each address to a graph
+
+vertex_1 = Vertex('WGU')
+truck_three_graph.add_vertex(vertex_1)
+vertex_2 = Vertex('Garden')
+truck_three_graph.add_vertex(vertex_2)
+vertex_3 = Vertex('Park')
+truck_three_graph.add_vertex(vertex_3)
+vertex_4 = Vertex('Taylorsville')
+truck_three_graph.add_vertex(vertex_4)
+
+
+truck_three_graph.add_undirected_edge(vertex_1, vertex_2, 7.2)
+truck_three_graph.add_undirected_edge(vertex_1, vertex_3, 3.8)
+truck_three_graph.add_undirected_edge(vertex_1, vertex_4, 11.0)
+
+truck_three_graph.add_undirected_edge(vertex_2, vertex_3, 7.1)
+truck_three_graph.add_undirected_edge(vertex_2, vertex_4, 6.4)
+truck_three_graph.add_undirected_edge(vertex_3, vertex_4, 9.2)
+
+closest_location = (nearest_neighbor(truck_three_graph, vertex_1))
+
+print(round(closest_location.total_distance))
+print('Following is printing result: ')
+
+
+for e in closest_location.ordered_list_delivery:
     print(e)
-print(truck_three.package_array)
--- nearest_neighbor.py
+++ nearest_neighbor.py
@@ -1,5 +1,11 @@
 import operator
-from Graph import Graph, Vertex
+from locations import Location
+
+
+class Nearest_Neighbor_Results:
+    def __init__(self, ordered_list_delivery, total_distance):
+        self.ordered_list_delivery = ordered_list_delivery
+        self.total_distance = total_distance
 
 
 class Vertex:
@@ -9,6 +15,7 @@ class Vertex:
         self.label = label
         self.distance = float('inf')
         self.pred_vertex = None
+        self.visited = False
 
     def __str__(self):
         return self.label
@@ -31,9 +38,99 @@ class Graph:
         self.add_directed_edge(vertex_b, vertex_a, weight)
 
 
+truck_three_graph = Graph()
+
+
+def nearest_neighbor(g, start_vertex):
+    order_to_visit = []
+    # Put all vertices in an unvisited queue.
+    unvisited_list = []
+
+    # total distance traveled for the truck
+    distance_traveled = 0
+
+    # add all vertexes to unvisited queue
+    for current_vertex in g.adjacency_list:
+        unvisited_list.append(current_vertex)
+        # make sure all vertexes are set to not visited
+        current_vertex.visited = False
+        # unvisited_queue = [vertex_1, vertex_2, ...]
+
+    # new design here
+    print(len(unvisited_list))
+    for e in unvisited_list:
+        print('Unvisited list: ' + str(e))
+    print()
+
+    current_vertex = start_vertex
+    order_to_visit.append(current_vertex)
+
+    # while the unvisited list is not empty
+    while len(unvisited_list) > 0:
+        # iterator
+        i = 1
+        print('Distance traveled: ' + str(distance_traveled))
+        print('Current vertex: ' + str(current_vertex))
+
+        # if the list is on the last item, that item must return to wgu
+        if len(unvisited_list) == 1:
+            closest_location_distance = g.edge_weights[(current_vertex, start_vertex)]
+            distance_traveled = distance_traveled + closest_location_distance
+            closest_location = start_vertex
+            order_to_visit.append(closest_location)
+            current_vertex.visited == True
+
+            delete_already_visited_vertex = current_vertex
+            unvisited_list.remove(delete_already_visited_vertex)
+            return Nearest_Neighbor_Results(order_to_visit, distance_traveled)
+
+        # find all vertexes adjacent to the start vertex
+        for vertex in g.adjacency_list[current_vertex]:
+            print(vertex)
+            # find distance between current vertex and vertex
+            if i == 1 and vertex.visited == False:
+                closest_location_distance = g.edge_weights[(current_vertex, vertex)]
+                closest_location = vertex
+                i = i + 1
+            else:
+                if g.edge_weights[(current_vertex, vertex)] < closest_location_distance and vertex.visited == False:
+                    closest_location_distance = g.edge_weights[(current_vertex, vertex)]
+                    closest_location = vertex
+                    i = i + 1
+            # print(vertex)
+        order_to_visit.append(closest_location)
+        current_vertex.visited = True
+
+        delete_already_visited_vertex = current_vertex
+
+        current_vertex = closest_location
+        print('Current vertex now: ' + str(current_vertex))
+
+        unvisited_list.remove(delete_already_visited_vertex)
+        distance_traveled = distance_traveled + closest_location_distance
+
+        print('Closest Location: ' + str(closest_location))
+        print('Closest location distance: ' + str(closest_location_distance))
+        print()
+    print()
+
+    return Nearest_Neighbor_Results(order_to_visit, distance_traveled)
+
 # obtain distance data from distances.csv
 # implement nearest neighbor algorithm
 
 def load_distances(fileName):
     with open(fileName, 'r') as file:
         distance_reader = csv.reader(file)
+
+
+def load_locations(fileName):
+    with open(fileName, 'r') as file:
+        location_reader = csv.reader(file)
+
+        for location in location_reader:
+            location_id = int(location[0])
+            location_name = location[1]
+            location_address = location[2]
+
+            location_obj = Location(location_id, location_name, location_address)
